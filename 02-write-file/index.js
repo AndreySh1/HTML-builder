bc8db5c9e5df05a72a4cf8c84cfedd3e55ed9fbc
unknown <shishlik19@gmail.com>
2022-05-25 00:11:31 +0300

    feat: corrected a flaw

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,18 +1,15 @@
 const fs = require('fs');
 const path = require('path');
-const { stdin, stdout } = process;
+const {stdin, stdout, exit} = process;
+
+const stream = fs.createWriteStream('./02-write-file/output.txt');
 
 
-process.on('SIGINT', function(){
-    process.exit();
-});
-fs.writeFile(
-      path.join('./02-write-file/output.txt'), '', (error) => {
-        if (error) throw error;
-        stdout.write('Файл создан\n');
-}
-);
-process.on('exit', () => stdout.write('До встречи\n'));
+stdout.write('Файл создан\n');
 stdin.on('data', data => {
-      if (data.toString().trim() === 'exit') process.exit();
+  if (data.toString().trim() === 'exit') exit();
+  stream.write(data.toString());
 });
+
+process.on('exit', () => stdout.write('До встречи\n'));
+process.on('SIGINT', exit);
